8ae43f58d1a098b58c0a52f61b8d5f4c766e06c9
koneone2002 <katieo3507@gmail.com>
2020-09-15 10:21:07 -0700

    collection page and route created

---
+++ src/pages/collection/Collection.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CollectionItem from '../../components/collectionItem/CollectionItem';
+import './collection.scss';
+
+const Collection = ({ match }) => {
+  return (
+    <div className='collection-page'>
+      <h2>COLLECTION PAGE</h2>
+    </div>
+  );
+};
+
+export default Collection;
--- src/pages/shop/ShopPage.js
+++ src/pages/shop/ShopPage.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import CollectionsOverview from '../../components/collectionsOverview/CollectionsOverview';
-
-const ShopPage = ({ collections }) => (
-  <div className='shop-page'>
-    <CollectionsOverview />
-  </div>
-);
-
+import Collection from '../../pages/collection/Collection';
+const ShopPage = ({ match }) => {
+  //console.log(match.path);
+  return (
+    <div className='shop-page'>
+      <Route exact path={`${match.path}`} component={CollectionsOverview} />
+      <Route path={`${match.path}/:collectionId`} component={Collection} />
+    </div>
+  );
+};
 export default ShopPage;
