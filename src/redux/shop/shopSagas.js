00830f098c48c2af65db1d263951ccaceec92efa
koneone2002 <katieo3507@gmail.com>
2020-11-17 15:05:24 -0800

    created saga and removed async store action

--- src/redux/shop/shopSagas.js
+++ src/redux/shop/shopSagas.js
@@ -1,14 +1,32 @@
-import { takeEvery } from 'redux-saga/effects';
+import { takeEvery, call, put } from 'redux-saga/effects';
 
 import { ShopActionTypes } from './shopTypes';
 
-function* fetchCollectionsAsync() {
-  yield console.log('I am fired');
+import {
+  firestore,
+  convertCollectionsSnapshotToMap
+} from '../../firebase/firebaseUtils';
+import {
+  fetchCollectionsSuccess,
+  fetchCollectionsFailure
+} from './shopActions';
+
+const { FETCH_COLLECTIONS_START } = ShopActionTypes;
+
+export function* fetchCollectionsAsync() {
+  try {
+    const collectionRef = firestore.collection('collections');
+    const snapshot = yield collectionRef.get();
+    const collectionsMap = yield call(
+      convertCollectionsSnapshotToMap,
+      snapshot
+    );
+    yield put(fetchCollectionsSuccess(collectionsMap));
+  } catch (error) {
+    yield put(fetchCollectionsFailure(error.message));
+  }
 }
 
 export function* fetchCollectionsStart() {
-  yield takeEvery(
-    ShopActionTypes.FETCH_COLLECTIONS_START,
-    fetchCollectionsAsync
-  );
+  yield takeEvery(FETCH_COLLECTIONS_START, fetchCollectionsAsync);
 }
